ca77e8f5df178b538b637cfab5cddf0ffef5a07c
rrex971 <75212090+rrex971@users.noreply.github.com>
2025-02-12 14:36:53 +0530

    Update font for search results

--- src/screens/SearchResults.jsx
+++ src/screens/SearchResults.jsx
@@ -5,7 +5,7 @@ import transition from "../transition";
 const SearchResults = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     return (
-        <div className="h-screen text-4xl flex justify-center pt-24">
+        <div className="h-screen text-4xl flex font-body justify-center pt-24">
             <div className="text-mercury-200 bg-woodsmoke-900 rounded-xl w-5/6 flex flex-col py-16 px-16 space-y-16">
                 <div className="font-bold pb-4">Search results for "{searchParams.get("q")}"</div>
                 <div className="text-2xl">No results :(</div>
